c1ddad25ffcd986dbc29bbf63dc804b716a2dca0
jwarmkessel <justin@rewardme.com>
2012-11-20 16:46:18 -0800

    Initial Commit

---
+++ Ninja/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  Ninja
+//
+//  Created by Justin Warmkessel on 11/20/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
