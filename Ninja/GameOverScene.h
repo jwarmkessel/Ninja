aecd6ae8ed2222663775125f0e246c34043ed3a7
jwarmkessel <justin@rewardme.com>
2012-11-29 01:02:02 -0800

    first commit

---
+++ Ninja/GameOverScene.h
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.h
+//  Ninja
+//
+//  Created by Justin Warmkessel on 11/20/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameOverScene : NSObject
+
+@end
---
+++ Ninja/GameOverScene.m
@@ -0,0 +1,13 @@
+//
+//  GameOverScene.m
+//  Ninja
+//
+//  Created by Justin Warmkessel on 11/20/12.
+//
+//
+
+#import "GameOverScene.h"
+
+@implementation GameOverScene
+
+@end
